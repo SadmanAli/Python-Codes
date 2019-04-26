5821c25f07b888de07c6abd3bd1bc50714952680
Sadman Ali Mishu <48767944+SadmanAli@users.noreply.github.com>
2019-04-26 02:33:42 -0700

    Add files via upload

---
+++ lcs.py
@@ -0,0 +1,27 @@
+def lcss(X, Y):
+
+	m = len(X)
+	n = len(Y)
+
+
+	L = [[None]*(n + 1) for i in xrange(m + 1)]
+
+	"""Following steps build L[m + 1][n + 1] in bottom up fashion 
+	Note: L[i][j] contains length of LCS of X[0..i-1] 
+	and Y[0..j-1]"""
+	for i in range(m + 1):
+		for j in range(n + 1):
+			if i == 0 or j == 0 :
+				L[i][j] = 0
+			elif X[i-1] == Y[j-1]:
+				L[i][j] = L[i-1][j-1]+1
+			else:
+				L[i][j] = max(L[i-1][j], L[i][j-1])
+
+
+	return L[m][n]
+
+
+X = "AGGTAB"
+Y = "GXTXAYB"
+print "Length of LCS is ", lcss(X, Y)
